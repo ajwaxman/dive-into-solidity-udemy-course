78da47f6e21ce8840450403bdb230bb6ce93daf1
Adam Waxman <awaxman11@gmail.com>
2022-09-04 20:33:22 -0400

    Knowledge test exercises

--- contracts/KnowledgeTest.sol
+++ contracts/KnowledgeTest.sol
@@ -5,9 +5,36 @@ pragma solidity >=0.5.0 <0.9.0;
 contract KnowledgeTest {
     string[] public tokens = ["BTC", "ETH"];
     address[] public players;
+    
+    address public owner;
 
-    function changeTokens() public view {
-        string[] memory t = tokens;
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function start() public {
+        players.push(msg.sender);
+    }
+
+    function concatenate(string memory s1, string memory s2) public pure returns(string memory) {
+        return string.concat(s1, s2);
+        // return string(abi.encodePacked(s1, s2));
+    }
+
+    function changeTokens() public {
+        string[] storage t = tokens;
         t[0] = "VET";
     }
+
+    function getBalance() public view returns (uint) {
+        return address(this).balance;
+    }
+
+    function transferAll(address payable _address) public {
+        require(msg.sender == owner, "ONLY_OWNER");
+        uint balance = getBalance(); 
+        _address.transfer(balance);
+    }
+
+    receive() external payable {}
 }
